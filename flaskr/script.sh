6416113524e5abd450f2a6e20254930f78bfc4a3
safar.radim <safar.radim@gmail.com>
2022-10-14 19:52:13 +0000

    Expanded script.sh

--- flaskr/script.sh
+++ flaskr/script.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-for var in "$@"
-do
-    python3 powerPlants.py user changeFinance "$var" 2000
-done
+print_help () {
+    echo "usage: script.sh advance"
+    echo "       script.sh add amount people..."
+    echo ""
+    echo "       amount - amount of finance to add"
+    echo "       people - receiving users"
+}
+
+case "$1" in
+    "add")
+        shift
+        amount="$1"
+        shift
+        while (("$#"))
+        do
+            python3 powerPlants.py user changeFinance "$1" "$amount"
+            echo "$1"
+            shift
+        done
+        ;;
+    "advance")
+        shift
+        amount=($(python3 powerPlants.py plant list | wc -l))
+        amount=$(( amount - 1 ))
+        i=1
+        while [ "$i" -le "$amount" ]
+        do
+            python3 powerPlants.py plant advance "$i"
+            i=$((i + 1))
+        done
+        ;;
+    *)
+        echo "Uknown arg"
+        print_help
+        ;;
+esac
